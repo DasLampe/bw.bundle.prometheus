92c088e33c7bebac4ae03aad774f60a86f21cc47
DasLampe <daslampe@lano-crew.org>
2023-10-29 16:37:59 +0100

    add: Collect nodes with node exporter from repository and add scrape_config.

--- metadata.py
+++ metadata.py
@@ -1,3 +1,8 @@
+from bundlewrap.metadata import DoNotRunAgain
+import re
+
+global repo, node
+
 defaults = {
     'prometheus': {
         'version': '2.43.0',
@@ -13,9 +18,16 @@ defaults = {
         'whitelisted_ips': [],
         'scrape_configs': [
             # {
-            #    "name": "node",
-            #    "targets": ["127.0.0.1:9100"],
-            # }
+            #     'name': 'prometheus',
+            #     'static_configs': [
+            #         {
+            #             'labels': {
+            #                 'hostname': 'localhost',
+            #             },
+            #             'targets': ['127.0.0.1:9090'],
+            #         },
+            #     ],
+            # },
         ],
         'additional_scrape_configs': [
             # {
@@ -45,3 +57,49 @@ def add_iptables(metadata):
             dest_port(metadata.get('prometheus').get('http').get('port'))
 
     return iptables_rules
+
+@metadata_reactor
+def find_node_exporter_hosts_to_monitor(metadata):
+    static_configs = []
+    relabel_configs = []
+    def get_targets(check_node):
+        target_interfaces = []
+        m = check_node.metadata
+        port = m.get('prometheus_node_exporter').get('http', {}).get('port', 9100)
+
+        interfaces = [m.get('main_interface'), ]
+        interfaces += m.get('prometheus_node_exporter').get('additional_interfaces', [])
+
+        for i in interfaces:
+            ip = m.get('interfaces', {}).get(i, {}).get('ip_addresses', [])
+            if len(ip) > 0:
+                target_interfaces += [f'{ip[0]}:{port}', ]
+
+        return target_interfaces
+
+    for checked_node in sorted(repo.nodes, key=lambda x: x.name):
+        if not checked_node.has_bundle('prometheus_node_exporter'):
+            continue
+        if checked_node.partial_metadata == {}:
+            continue
+
+        static_configs += [{
+            'labels': {
+                'hostname': checked_node.hostname,
+                'os': checked_node.os,
+                'instance': checked_node.name,
+                'instance_ip': get_targets(checked_node)[0].split(':',1)[0],
+            },
+            'targets': get_targets(checked_node)
+        }, ]
+
+    return {
+        'prometheus': {
+            'scrape_configs': [
+                {
+                    'name': 'node_exporter',
+                    'static_configs': static_configs,
+                },
+            ],
+        },
+    }
